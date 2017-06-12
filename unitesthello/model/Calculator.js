e8cd80c7f00933c7adf3810eb3a335b8a1e9986f
Bryan Estevez <bryan_estevez14@yahoo.com.ph>
2017-06-12 18:15:50 +0800

    with multiply

--- unitesthello/model/Calculator.js
+++ unitesthello/model/Calculator.js
@@ -1,7 +1,22 @@
 let Calculator = {
-  add : (num1, num2) =>{
-    return num1 + num2
-  }
+  add : (numbers) =>{
+     if(!numbers.num1){
+          throw "num1 should have a value"
+     }else if (!numbers.num2) {
+         throw "num2 should have a value"
+     }
+
+    return numbers.num1 + numbers.num2
+  },
+
+  multiply : (numbers) =>{
+       if(!numbers.num1){
+           throw "num1 should have a value"
+      }else if (!numbers.num2) {
+          throw "num2 should have a value"
+      }
+       return numbers.num1 * numbers.num2
+ }
 
 }
 
--- unitesthello/test/CalculatorTest.js
+++ unitesthello/test/CalculatorTest.js
@@ -1,13 +1,60 @@
 let chai = require("chai")
 let Calculator = require("../model/Calculator")
-
+let sinon = require('sinon')
 chai.should()
 
+
 describe("Calculator", ()=>{
+
   describe("add", ()=>{
     it("should add numbers", ()=>{
-      let sum = Calculator.add(3,5)
+      let sum = Calculator.add({num1:3,num2:5})
       sum.should.equal(8)
+
     })
+
+    context("When num1 does not exist",()=>{
+         it("should throw an error", ()=>{
+              let addition = ()=>{
+                   let sum = Calculator.add({num2:5})
+              }
+          addition.should.throw("num1 should have a value")
+         })
+    })
+
+    context("When num2 does not exist",()=>{
+         it("should throw an error", ()=>{
+              let addition = ()=>{
+                   let sum = Calculator.add({num1:5})
+              }
+          addition.should.throw("num2 should have a value")
+         })
+    })
+  })
+
+  describe("multiply", ()=>{
+    it("should multiply a number", ()=>{
+      let sum = Calculator.multiply({num1:3,num2:5})
+      sum.should.equal(15)
+    })
+
+    context("When num1 does not exist",()=>{
+         it("should throw an error", ()=>{
+              let multiply = ()=>{
+                   let product = Calculator.multiply({num2:5})
+              }
+          multiply.should.throw("num1 should have a value")
+         })
+    })
+
+    context("When num2 does not exist",()=>{
+         it("should throw an error", ()=>{
+              let multiply = ()=>{
+                   let product = Calculator.multiply({num1:5})
+              }
+          multiply.should.throw("num2 should have a value")
+         })
+    })
+
   })
 })
