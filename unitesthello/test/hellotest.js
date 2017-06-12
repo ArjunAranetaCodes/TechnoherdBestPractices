cffe8f171b207205302202b0cec4fdc03e6ae3dd
Bryan Estevez <bryan_estevez14@yahoo.com.ph>
2017-06-12 17:26:36 +0800

    it should be a string

--- unitesthello/test/hellotest.js
+++ unitesthello/test/hellotest.js
@@ -9,6 +9,13 @@ describe("Stable Run",()=>{
     it("should output hello world", ()=>{
       let result = hello.output()
       result.should.equal("hello world")
+
+    })
+
+    it("should output must be a string", ()=>{
+      let result = hello.output()
+      result.should.be.a("string")
+
     })
   })
 })
